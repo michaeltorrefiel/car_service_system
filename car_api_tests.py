031535283463767c6489674b21eaaf885eeb16c5
mike <michael.torrefielc@gmail.com>
2024-12-12 23:51:04 +0800

    add test for get mechanic endpoint and function

--- car_api_tests.py
+++ car_api_tests.py
@@ -7,8 +7,8 @@ class CarApiTests(unittest.TestCase):
         self.app = app.test_client()
 
         self.test_customer_dummy = {
-            "first_name": "TEST",
-            "last_name": "TESTER",
+            "first_name": "CUSTOMER_TEST",
+            "last_name": "CUSTOMER_TESTER",
             "contact_number": "09000735700"
         } 
         response = self.app.post("/customers", json=self.test_customer_dummy)
@@ -16,8 +16,20 @@ class CarApiTests(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
         self.customer_id = response.get_json()["customer_id"]
 
+        self.test_mechanic_dummy = {
+            "first_name": "MECHANIC_TEST",
+            "last_name": "MECHANIC_TESTER",
+            "contact_number": "09110735701",
+            "other_mechanic_details": "TEST TEST TEST"
+        }
+        response = self.app.post("/mechanics", json=self.test_mechanic_dummy)
+        # print(response.get_json())
+        self.assertEqual(response.status_code, 201)
+        self.mechanic_id = response.get_json()["mechanic_id"]
+
     def tearDown(self):
         self.app.delete(f"/customers/{self.customer_id}")
+        self.app.delete(f"/mechanics/{self.mechanic_id}")
 
     def test_get_customer(self):
         response = self.app.get(f"/customers/{self.customer_id}")
@@ -28,6 +40,17 @@ class CarApiTests(unittest.TestCase):
         self.assertEqual(data[0]["first_name"], self.test_customer_dummy["first_name"])
         self.assertEqual(data[0]["last_name"], self.test_customer_dummy["last_name"])
         self.assertEqual(data[0]["contact_number"], self.test_customer_dummy["contact_number"])
+
+    def test_get_mechanic(self):
+        response = self.app.get(f"/mechanics/{self.mechanic_id}")
+        self.assertEqual(response.status_code, 200)
+
+        data = response.get_json()
+        # print(data)
+        self.assertEqual(data[0]["first_name"], self.test_mechanic_dummy["first_name"])
+        self.assertEqual(data[0]["last_name"], self.test_mechanic_dummy["last_name"])
+        self.assertEqual(data[0]["contact_number"], self.test_mechanic_dummy["contact_number"])
+        self.assertEqual(data[0]["other_mechanic_details"], self.test_mechanic_dummy["other_mechanic_details"])
 
     def test_home_page(self):
         response = self.app.get("/")
