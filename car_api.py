250194c954d63fc1a4234d55b1289b0cf1db0aa8
mike <michael.torrefielc@gmail.com>
2024-12-14 01:37:27 +0800

    refactor post endpoints and functions

--- car_api.py
+++ car_api.py
@@ -47,10 +47,13 @@ def get_records_by_id(table, id):
 
     if table == "customers":
         id_type = "customer_id"
+
     elif table == "mechanics":
         id_type = "mechanic_id"
+
     else:
         id_type = "booking_id"
+        
     rows = query_exec(f"select * from {table} where {id_type}= {id}")
     if not rows:
         return make_response(jsonify({"error": "No records found"}), 404)
@@ -69,79 +72,64 @@ def get_car_records_by_id(plate_number):
     
     return make_response(jsonify(rows), 200)
 
-@app.route("/customers", methods=["POST"])
-def add_customer_records():
+@app.route("/<table>", methods=["POST"])
+def add_records(table):
     cur = mysql.connection.cursor()
 
+    db_tables = ["customers", "mechanics", "cars", "bookings"]
+    if table not in db_tables:
+        return make_response(jsonify({"error": "Table not found"}), 404)
+
     info = request.get_json()
-    first_name = info["first_name"]
-    last_name = info["last_name"]
-    contact_number = info["contact_number"]
-    cur.execute("insert into customers(first_name, last_name, contact_number) value (%s, %s, %s)", (first_name, last_name, contact_number))
+    if table == "customers":
+        first_name = info["first_name"]
+        last_name = info["last_name"]
+        contact_number = info["contact_number"]
+        cur.execute("insert into customers(first_name, last_name, contact_number) value (%s, %s, %s)", (first_name, last_name, contact_number))
+        record_name = "customer"
+        id_type = "customer_id"
+        id = cur.lastrowid
+
+
+    elif table == "mechanics":
+        first_name = info["first_name"]
+        last_name = info["last_name"]
+        contact_number = info["contact_number"]
+        other_mechanic_details = info.get("other_mechanic_details", "")
+        cur.execute("insert into mechanics(first_name, last_name, contact_number, other_mechanic_details) value (%s, %s, %s, %s)", (first_name, last_name, contact_number, other_mechanic_details))
+        record_name = "mechanic"
+        id_type = "mechanic_id"
+        id = cur.lastrowid
+
+
+    elif table == "cars":
+        plate_number = info["plate_number"]
+        customer_id = info["customer_id"]
+        manufacturer = info["manufacturer"]
+        model = info["model"]
+        known_issue = info.get("known_issue", "")
+        other_details = info.get("other_details", "")
+        cur.execute("insert into cars(plate_number, customer_id, manufacturer, model, known_issue, other_details) value (%s, %s, %s, %s, %s, %s)", (plate_number, customer_id, manufacturer, model, known_issue, other_details))
+        record_name = "car"
+        id_type = "plate_number"
+        id = plate_number
+
+    else:
+        mechanic_id = info["mechanic_id"]
+        customer_id = info["customer_id"]
+        plate_number = info["plate_number"]
+        date_time_of_service = info["date_time_of_service"]
+        payment = info["payment"]
+        cur.execute("insert into bookings(mechanic_id, customer_id, plate_number, date_time_of_service, payment) value (%s, %s, %s, %s, %s)", (mechanic_id, customer_id, plate_number, date_time_of_service, payment))
+        record_name = "booking"
+        id_type = "booking_id"
+        id = cur.lastrowid
 
     mysql.connection.commit()
     rows_affected = cur.rowcount
-    customer_id = cur.lastrowid
     cur.close()
 
-    return make_response(jsonify({"message": "customer added successfully", "rows_affected": rows_affected, "customer_id": customer_id}), 201)
-
-@app.route("/mechanics", methods=["POST"])
-def add_mechanic_records():
-    cur = mysql.connection.cursor()
-
-    info = request.get_json()
-    first_name = info["first_name"]
-    last_name = info["last_name"]
-    contact_number = info["contact_number"]
-    other_mechanic_details = info.get("other_mechanic_details", "")
-    cur.execute("insert into mechanics(first_name, last_name, contact_number, other_mechanic_details) value (%s, %s, %s, %s)", (first_name, last_name, contact_number, other_mechanic_details))
-
-    mysql.connection.commit()
-    rows_affected = cur.rowcount
-    mechanic_id = cur.lastrowid
-    cur.close()
-
-    return make_response(jsonify({"message": "mechanic added successfully", "rows_affected": rows_affected, "mechanic_id": mechanic_id}), 201)
-
-@app.route("/cars", methods=["POST"])
-def add_car_records():
-    cur = mysql.connection.cursor()
-
-    info = request.get_json()
-    plate_number = info["plate_number"]
-    customer_id = info["customer_id"]
-    manufacturer = info["manufacturer"]
-    model = info["model"]
-    known_issue = info.get("known_issue", "")
-    other_details = info.get("other_details", "")
-    cur.execute("insert into cars(plate_number, customer_id, manufacturer, model, known_issue, other_details) value (%s, %s, %s, %s, %s, %s)", (plate_number, customer_id, manufacturer, model, known_issue, other_details))
-
-    mysql.connection.commit()
-    rows_affected = cur.rowcount
-    cur.close()
-
-    return make_response(jsonify({"message": "car added successfully", "rows_affected": rows_affected, "plate_number": plate_number}), 201)
-
-@app.route("/bookings", methods=["POST"])
-def add_booking_records():
-    cur = mysql.connection.cursor()
-
-    info = request.get_json()
-    mechanic_id = info["mechanic_id"]
-    customer_id = info["customer_id"]
-    plate_number = info["plate_number"]
-    date_time_of_service = info["date_time_of_service"]
-    payment = info["payment"]
-
-    cur.execute("insert into bookings(mechanic_id, customer_id, plate_number, date_time_of_service, payment) value (%s, %s, %s, %s, %s)", (mechanic_id, customer_id, plate_number, date_time_of_service, payment))
-
-    mysql.connection.commit()
-    rows_affected = cur.rowcount
-    booking_id = cur.lastrowid
-    cur.close()
-
-    return make_response(jsonify({"message": "booking added successfully", "rows_affected": rows_affected, "booking_id": booking_id}), 201)
+    return make_response(jsonify({"message": f"{record_name} added successfully", "rows_affected": rows_affected, id_type: id}), 201)
 
 @app.route("/customers/<int:customer_id>", methods = ["PUT"])
 def edit_customer_records(customer_id):
